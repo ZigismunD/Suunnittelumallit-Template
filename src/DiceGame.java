eb021c7412d15ba001e6f5dfc30923a75736f42e
Sami Sikkilä <sami.sikkila@metropolia.fi>
2019-04-09 20:48:29 +0300

    Valmis

---
+++ src/DiceGame.java
@@ -0,0 +1,37 @@
+import java.util.Random;
+
+public class DiceGame extends Game {
+    private int[] pisteet;
+    Random rand;
+    private boolean isEnd = false;
+    private String voittaja = "";
+
+    @Override
+    void initializeGame() {
+        pisteet = new int[playersCount];
+        rand = new Random();
+    }
+
+    @Override
+    void makePlay(int player) {
+        pisteet[player] += rand.nextInt(6) + 1;
+        if (pisteet[player] >= 30) {
+            voittaja = "Voittaja: Pelaaja " + (player + 1)  + ", pisteillä: " + pisteet[player];
+            setEnd();
+        }
+    }
+
+    @Override
+    boolean endOfGame() {
+        return isEnd;
+    }
+
+    @Override
+    void printWinner() {
+        System.out.println(voittaja);
+    }
+
+    private void setEnd() {
+        this.isEnd = true;
+    }
+}
